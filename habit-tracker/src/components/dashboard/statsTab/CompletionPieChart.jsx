96bf80b6fb346a13acae522fbca05c5025b055af
Furkan Deniz <dnzfurkan@outlook.com>
2025-06-20 16:02:44 +0300

    fixed folder name

---
+++ habit-tracker/src/components/dashboard/statsTab/CompletionPieChart.jsx
@@ -0,0 +1,50 @@
+import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from "recharts";
+import { useGetHabitsQuery } from "../../../features/habits/habitApi";
+
+
+
+const COLORS = ['#22c55e', '#ef4444'];
+
+
+export const CompletionPieChart = () => {
+
+
+    const { data } = useGetHabitsQuery();
+
+    const habits = data?.value;
+
+   
+
+
+    const completedCount =habits ? habits?.filter(h => h.isCompletedToday).length :0;
+    const incompletedCount = habits ? habits?.length - completedCount:0;
+    const chartData = [
+        { name: 'Tamamlanan', value: completedCount },
+        { name: 'Eksik kalan', value: incompletedCount }
+    ];
+
+    return (
+        <div className="flex flex-col items-center justify-center bg-gray-900 p-6 rounded-2xl shadow-md w-full  mx-auto mt-6  h-[400px] ">
+            <h2 className="text-xl font-bold text-white mb-4 ">Günlük tamamlama oranı</h2>
+                <PieChart width={250} height={250} className="px-4 ">
+                    <Pie
+                        data={chartData}
+                        cx='50%'
+                        cy='50%'
+                        innerRadius={60}
+                        outerRadius={90}
+                        paddingAngle={5}
+                        dataKey='value'
+                        label
+                        labelPosition="outside"
+                    >
+                        {chartData.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index]}></Cell>
+                        ))}
+                    </Pie>
+                    <Legend />
+                </PieChart>
+            
+        </div>
+    );
+}
